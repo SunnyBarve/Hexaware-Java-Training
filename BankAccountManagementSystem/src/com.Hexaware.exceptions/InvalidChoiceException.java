2c4970f2791b6473ce4d0fc53b48fb01997655c3
Sunny-Barve <111621638+SunnyBarve@users.noreply.github.com>
2024-10-03 08:25:11 +0530

    Add files via upload

---
+++ BankAccountManagementSystem/src/com.Hexaware.exceptions/InvalidChoiceException.java
@@ -0,0 +1,8 @@
+package com.Hexaware.exceptions;
+
+public class InvalidChoiceException extends Exception {
+	 
+	 public InvalidChoiceException(String message) {
+	        super(message);
+	    }
+}
