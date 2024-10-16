069f0ad0993d19318d7fee696badced4c4674088
Sunny-Barve <111621638+SunnyBarve@users.noreply.github.com>
2024-10-16 12:45:15 +0530

    Add files via upload

---
+++ ShopSmart/src/InsufficientPaymentException.java
@@ -0,0 +1,7 @@
+package com.hexaware.exception;
+
+public class InsufficientPaymentException extends Exception {
+	public InsufficientPaymentException(String message) {
+        super(message);
+    }
+}
